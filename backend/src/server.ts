6dac432966259eb6850a7732afc723aeaada44ed
Marcelo Henrique Aguiar Marques <96503481+MHAMarques@users.noreply.github.com>
2023-05-01 10:20:08 -0300

    Back Feat: Install Socket.IO
    Instalar e configurar Socket.IO no backend

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,5 +1,12 @@
 import app from "./app";
+import http from 'http';
+import { Server } from 'socket.io';
 import AppDataSource from "./data-source";
+
+const server = http.createServer(app);
+//const io = new Server(server);
+const cors = require('cors');
+const io = new Server(server, { cors: { origin: '*' } });
 
 (async () => {
 
@@ -7,8 +14,23 @@ import AppDataSource from "./data-source";
     .catch((err) => {
         console.error("Error to load API Data Source", err)
     })
-    
-    app.listen(3030, () => {
-        console.log("Chatinip API Running on Port: 3030")
-    })    
-})()
+
+
+    io.on('connection', (socket) => {
+        console.log('User connected:', socket.id);
+
+        socket.on('message', (message) => {
+        console.log('Received message:', message);
+        io.emit('message', message);
+        });
+
+        socket.on('disconnect', () => {
+        console.log('User disconnected:', socket.id);
+        });
+    });
+
+  server.listen(3030, () => {
+    console.log('Chatinip API Running on Port: 3030');
+  });
+
+})();
